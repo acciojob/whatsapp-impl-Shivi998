dc7287fddb0779e7dde090d26287519db4aa125b
Shivi998 <shivani.rmlu2022@gmail.com>
2023-06-24 22:44:15 +0530

    done

--- src/main/java/com/driver/Application.java
+++ src/main/java/com/driver/Application.java
@@ -1,7 +1,8 @@
+
 package com.driver;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
@@ -10,3 +11,4 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 }
+
